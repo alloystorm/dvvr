9f4062f6294b1113f3d73b2e2ed48826bd468eed
alloystorm <alloystorm@gmail.com>
2023-11-04 22:45:37 +1100

    Fix navigation links

--- script/combine_navigation.py
+++ script/combine_navigation.py
@@ -15,7 +15,7 @@ for file, suffix in base_path.items():
     with open(file, 'r', encoding='utf-8') as f:
         content = f.read()
         if (len(suffix) > 0):
-            content = content.replace("---", "").replace("docs", "docs-" + suffix).replace("releases", "releases-" + suffix).replace("dancexr/", suffix + "/dancexr/")
-        combined += content
+            content = content.replace("docs", "docs-" + suffix).replace("releases", "releases-" + suffix).replace("dancexr/", suffix + "/dancexr/")
+        combined += content.replace("---", "")
 with open("_data/navigation.yml", 'w', encoding='utf-8') as f:
     f.write(combined)
