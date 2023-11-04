b7fb2a857185d08dea5c1758f2741ed49a058046
alloystorm <alloystorm@gmail.com>
2023-11-04 22:41:25 +1100

    Fix navigation links

--- script/combine_navigation.py
+++ script/combine_navigation.py
@@ -4,16 +4,18 @@ import re
 # Base path
 base_path = {
     "dancexr/navigation.md": "",
-    "zh/dancexr/navigation.md": "-zh",
-    "tw/dancexr/navigation.md": "-tw",
-    "jp/dancexr/navigation.md": "-jp",
-    "kr/dancexr/navigation.md": "-kr"
+    "zh/dancexr/navigation.md": "zh",
+    "tw/dancexr/navigation.md": "tw",
+    "jp/dancexr/navigation.md": "jp",
+    "kr/dancexr/navigation.md": "kr"
 }
 
 combined = ""
 for file, suffix in base_path.items():
     with open(file, 'r', encoding='utf-8') as f:
         content = f.read()
-        combined += content.replace("---", "").replace("docs", "docs" + suffix).replace("releases", "releases" + suffix)
+        if (len(suffix) > 0):
+            content = content.replace("---", "").replace("docs", "docs-" + suffix).replace("releases", "releases-" + suffix).replace("dancexr/", suffix + "/dancexr/")
+        combined += content
 with open("_data/navigation.yml", 'w', encoding='utf-8') as f:
     f.write(combined)
